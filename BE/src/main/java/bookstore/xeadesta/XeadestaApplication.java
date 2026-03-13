9793259edc151e966cda8b772c3f3e06f7dc2499
JonathanZefanya <jonathan.zefanya16@gmail.com>
2026-03-13 22:09:02 +0700

    first commit

---
+++ BE/src/main/java/bookstore/xeadesta/XeadestaApplication.java
@@ -0,0 +1,13 @@
+package bookstore.xeadesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XeadestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XeadestaApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/bookstore/xeadesta/XeadestaApplicationTests.java
@@ -0,0 +1,13 @@
+package bookstore.xeadesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XeadestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
